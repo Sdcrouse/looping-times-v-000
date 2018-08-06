36e161749a4e80014c0a52ebc287e755ee5a608b
Steven Crouse <sdcrouse91@gmail.com>
2018-08-06 19:36:56 +0000

    automatically backed up by learn

--- times.rb
+++ times.rb
@@ -1,6 +1,9 @@
 def using_times
 	#your code here
-	number = 7
+	number = 7 
+	# Not required, but I'm writing this as a reminder that 
+	# I can send a variable to #times as long as it's an integer.
+	
 	number.times do
 	  puts "Wingardium Leviosa"
 	end
